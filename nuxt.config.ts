b1955fee5fd860202b717daf2af43f8f703621d6
nguyentubadat <nguyentubadat833@gmail.com>
2025-08-15 15:39:53 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxt/image", "@nuxt/ui"],
+  srcDir: "./web",
+  serverDir: "./server",
+  content: {
+    experimental: { sqliteConnector: "native" },
+  },
+});
---
+++ web/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+
+useSeoMeta({
+  title: home.value?.title,
+  description: home.value?.description
+})
+
+</script>
+
+<template>
+  <div>
+    <ContentRenderer v-if="home" :value="home" />
+    <div v-else>Home not found</div>
+  </div>
+</template>
